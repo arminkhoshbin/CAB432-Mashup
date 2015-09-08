20946eca782977c3851e0cd08d626cb6ed5113e7
Armin <armin@zippy.com.au>
2015-09-09 00:49:52 +1000

    massive refactoring and add comments

--- Module/googleCalendar.js
+++ Module/googleCalendar.js
@@ -2,42 +2,46 @@ var google = require('googleapis');
 var googleAuth = require('google-auth-library');
 
 var SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
-var GoogleCalendar = {};
+
+GoogleCalendar = function() {
+}
+
 /**
  * Create an OAuth2 client with the given credentials.
  *
  * @param {Object} credentials The authorization client credentials.
  */
-GoogleCalendar.init = function (credentials) {
-  var clientSecret = credentials.client_secret;
-  var clientId = credentials.client_id;
-  var redirectUrl = credentials.redirect_uris[0];
+GoogleCalendar.prototype.oauth = function (credentials) {
+  this.clientSecret = credentials.client_secret;
+  this.clientId = credentials.client_id;
+  this.redirectUrl = credentials.redirect_uris[0];
   var auth = new googleAuth();
-  var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+  var oauth2Client = new auth.OAuth2(this.clientId, this.clientSecret, this.redirectUrl);
   return oauth2Client;
 }
 
 /**
  * Generate a new authentication URL.
  *
- * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
+ * @param {google.auth.OAuth2} oauth2Client An authorized OAuth2 client.
+ * @param {callback} callback Callback function
  */
-GoogleCalendar.getAuthUrl = function (oauth2Client) {
+GoogleCalendar.prototype.getAuthUrl = function (oauth2Client, callback) {
   var authUrl = oauth2Client.generateAuthUrl({
     access_type: 'offline',
     scope: SCOPES
   });
-  return authUrl;
+  callback(null, authUrl);
 }
 
 /**
  * Generate a token using the OAuth Client and authorization code.
  *
- * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
- * @param {String} authorization code
- * @param {callback} callback function
+ * @param {google.auth.OAuth2} oauth2Client An authorized OAuth2 client.
+ * @param {String} code Authorization code
+ * @param {callback} callback Callback function
  */
-GoogleCalendar.generateToken = function (oauth2Client, code, callback) {
+GoogleCalendar.prototype.generateToken = function (oauth2Client, code, callback) {
   oauth2Client.getToken(code, function(err, token) {
     if (err) {
       console.log('Error while trying to retrieve access token', err);
@@ -48,11 +52,13 @@ GoogleCalendar.generateToken = function (oauth2Client, code, callback) {
 }
 
 /**
- * Lists the next 10 events on the user's primary calendar.
+ * Lists the last 10 events on the user's primary calendar.
  *
- * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ * @param {google.auth.OAuth2} oauth2Client An authorized OAuth2 client.
+ * @param {callback} callback Callback function
  */
-GoogleCalendar.listEvents = function (auth) {
+GoogleCalendar.prototype.getEvents = function (auth, callback) {
+  var eventData;
   var calendar = google.calendar('v3');
   calendar.events.list({
     auth: auth,
@@ -63,20 +69,33 @@ GoogleCalendar.listEvents = function (auth) {
     orderBy: 'startTime'
   }, function(err, response) {
     if (err) {
-      console.log('The API returned an error: ' + err);
+      callback(err, null);
       return;
     }
     var events = response.items;
-    if (events.length == 0) {
-      console.log('No upcoming events found.');
-    } else {
-      console.log('Upcoming 10 events:');
-      for (var i = 0; i < events.length; i++) {
-        var event = events[i];
-        var start = event.start.dateTime || event.start.date;
-        console.log('%s - %s', start, event.summary);
-      }
+    callback(null, events);
+  });
+}
+
+/**
+ * Get specific event information by event ID.
+ *
+ * @param {google.auth.OAuth2} oauth2Client An authorized OAuth2 client.
+ * @param {string} eventID Event ID to get the data for.
+ * @param {callback} callback Callback function
+ */
+GoogleCalendar.prototype.getEventByID = function (auth, eventID, callback) {
+  var calendar = google.calendar('v3');
+  calendar.events.get({
+    auth: auth,
+    calendarId: 'primary',
+    eventId: eventID,
+  }, function(err, response) {
+    if (err) {
+      callback(err, null);
+      return;
     }
+    callback(null, response);
   });
 }
 
--- server.js
+++ server.js
@@ -1,42 +1,172 @@
 //express is a light weight web framework similar to sinatra (ruby), and Nancy (.net)
 //read more about express at: http://expressjs.com/
 var express = require('express');
-var secret = require('./config/config').GoogleCalendar;
+var secret = require('./config/config');
+var session = require('express-session');
+var async = require('async');
 
+
+// Google Maps GeoCoding API initialization
+var geocoderProvider = 'google';
+var httpAdapter = 'http';
+// optionnal
+var extra = {
+};
+var geocoder = require('node-geocoder')(geocoderProvider, httpAdapter. extra);
+
+// Google Calendar API initialization
 var GoogleCalendar = require('./Module/googleCalendar');
+var GoogleCalendarAPI = new GoogleCalendar();
+var GoogleCalendarOAuthClient = GoogleCalendarAPI.oauth(secret.GoogleCalendar);
+
+// Forecast.io API initialization
+var Forecast = require('./Module/forecast');
+var ForecastAPI = new Forecast(secret.Forecastio.api_key);
+
+// Uber API initialization
+var UberAPI = require('./Module/uber');
+var Uber = new UberAPI();
+var uber = Uber.init(secret.Uber);
 
 var app = express();
 
-var GoogleCalendarOAuthClient = GoogleCalendar.init(secret);
+// Session configuarion
+app.use(session({ 
+	secret: secret.GoogleCalendar.client_secret, 
+	resave: false,
+	saveUninitialized: true,
+	// valid for 1 hour
+	cookie: { maxAge: 3600000 },
+}));
 
-app.get('/', function(req, res) {
-	/* Add some session handling here for google authorization you glob! */
-	if (typeof GoogleCalendarOAuthClient.credentials.access_token != 'undefined') {
-		res.send("We have a token! \n" + GoogleCalendarOAuthClient.credentials.access_token);
-	}
-	else { res.redirect('/login'); }
+// Setting up the template engine
+app.set('view engine', 'ejs');
+app.set('view options', { layout: false });
+app.set('views', __dirname + '/views');
+
+// All the public files (client side files)
+app.use('/public', express.static('public'));
+
+// Handles Authentication to endpoints
+function requireAuthentication(req, res, next) {
+  if (req.session.loggedIn) {
+    next();
+  } else {
+    res.redirect('/login');
+  }
+}
+
+app.get('/', requireAuthentication, function(req, res) {
+	res.render('index');
 });
 
+// uber services available around a specific location.
+app.get('/uber/services', requireAuthentication, function(req, res) {
+	var address = req.query.address;
+	var endLat = req.query.endLat;
+	var endLong = req.query.endLong;
+	var addressLocation = {};
+	var result = {};
+
+	async.series([
+		function(callback) {
+			geocoder.geocode(address + ' Brisbane', function(err, data) {
+				addressLocation = data[0];
+				callback();
+			});
+		},
+		function(callback) {
+			uber.estimates.price({ 
+			  start_latitude: addressLocation.latitude, start_longitude: addressLocation.longitude, 
+			  end_latitude: endLat, end_longitude: endLong 
+			}, function (err, data) {
+			  if (err) console.error(err);
+			  result = data;
+			  callback();
+			});
+		},
+		], function(err) { 
+        if (err) return next(err);
+        res.send(result);
+  });
+
+});
+
+// Handles Google Authentication and setting session
 app.get('/login', function(req, res) {
 	if (req.query.hasOwnProperty('code')) {
-		GoogleCalendar.generateToken(GoogleCalendarOAuthClient, req.query.code, function(err, data) {
+		GoogleCalendarAPI.generateToken(GoogleCalendarOAuthClient, req.query.code, function(err, data) {
 			var token = data;
 			GoogleCalendarOAuthClient.credentials = token;
+			req.session.loggedIn = true;
 			res.redirect('/');
 		});
 	}
 	else {
 		// Authorize the app to use Google Calendar.
-		var authUrl = GoogleCalendar.getAuthUrl(GoogleCalendarOAuthClient);
-		//console.log(authUrl);
-		res.redirect(authUrl);
+		GoogleCalendarAPI.getAuthUrl(GoogleCalendarOAuthClient, function(err, data) {
+			var authUrl = data;
+			res.redirect(authUrl);
+		});
 	}
 });
 
+// Log out of dashboard and destroy the session.
+app.get('/logout', requireAuthentication, function(req, res) {
+	req.session.destroy(function(err) {
+  	if (err) { res.send(err); }
+  	else { res.redirect('/'); }
+	});
+});
 
+// get last 10 events in your calendar.
+app.get('/events', requireAuthentication, function(req, res) {
+	GoogleCalendarAPI.getEvents(GoogleCalendarOAuthClient, function(err, data) {
+		res.send(data);
+	});
+});
+
+// More detail about events including the weather forecast of the day.
+app.get('/event/:id', requireAuthentication, function(req, res) {
+	var eventDetail = {};
+	var eventLocation = {};
+	var weatherForecast = {};
+	async.series([
+		function(callback) {
+			GoogleCalendarAPI.getEventByID(GoogleCalendarOAuthClient, req.params.id, function(err, data) {
+				eventDetail = data;
+				callback();
+			});
+		},
+		function(callback) {
+			geocoder.geocode(eventDetail.location + ' Brisbane', function(err, data) {
+				eventLocation = data[0];
+				callback();
+			});
+		},
+		function(callback) {
+			ForecastAPI.getForecastByDate(eventLocation.latitude, eventLocation.longitude, eventDetail.start.dateTime, function(err, data) {
+				weatherForecast = JSON.parse(data).daily;
+				callback();
+			});
+		},
+		], function(err) { 
+        if (err) return next(err);
+        res.render('event', {'eventDetail': eventDetail, 'eventLocation': eventLocation, 'Weather': weatherForecast});
+  });
+});
+
+// This end point is for test purposes only
+app.get('/session/set', function(req, res) {
+	req.session.loggedIn = true;
+});
+
+// Starting the server
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('Example app listening at http://localhost:3000');
 });
+
+module.exports = app;
